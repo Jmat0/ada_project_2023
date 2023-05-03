52db12737b9d4ce7ff808d6e7da92a7347ebc771
Jmat0 <104056051+Jmat0@users.noreply.github.com>
2023-05-03 12:04:23 +0200

    Cleaning
    - Create order for files
    - Create copies we can work on
    - Archives

--- Copy_safe_to_work/4_mapping copie.py
+++ Copy_safe_to_work/4_mapping copie.py
@@ -6,12 +6,14 @@ import re
 char_df = pd.read_csv('cleaned_watch_text_def.csv')
 pattern = r' \(aka.*\)'
 char_df['Reference'] = char_df['Reference'].apply(lambda x: re.sub(pattern, '', x))
+char_df['Reference'] = char_df['Reference'].str.replace('/', '-')
 
 # Create a new column with the image file names
 char_df['image_file'] = char_df['Reference'].apply(lambda x: f"{x}.jpg")
+char_df['image_file_processed'] = 'processed_' + char_df['image_file']
 
 # Define the directory where the image files are stored
-image_dir = '/Users/julian/Desktop/Watches_Images'
+image_dir = '/Users/lawrencejesudasan/Downloads/Watches_Images'
 
 # Create a dictionary to map the characteristics to the images
 char_to_image = {}
@@ -26,13 +28,6 @@ for subdir, _, files in os.walk(image_dir):
 mapped_df = pd.DataFrame.from_dict(char_to_image, orient='index')
 
 mapped_df.to_csv("data_with_images.csv", index=False)
-
-# Check if the number of files in the image directory matches the number of rows in the characteristics dataset
-#if len(os.listdir(image_dir)) != len(char_df):
-    # Remove the extra rows in the characteristics dataset
-    #char_df = char_df[char_df['Reference'].isin(char_to_image.keys())]
-
-#char_df.to_csv("data_with_images.csv", index=False)
 
 # Print the dictionary to verify the mapping
 print(char_to_image)
