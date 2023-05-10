25661671c3149398fa9beef010fa3dd3e51af80c
lawrencejesudasan <79648932+lawrencejesudasan@users.noreply.github.com>
2023-05-10 16:00:05 +0200

    update 6_

--- 6_grayscale_color_feature.py
+++ 6_grayscale_color_feature.py
@@ -1,29 +1,50 @@
 import os
-import cv2
+from PIL import Image
 import numpy as np
+import pandas as pd
 
-# Set the directory path containing the subdirectories with the images
-dir_path = "/Users/lawrencejesudasan/Downloads/Watches_Images_Processed_copy"
+# Set the directory path containing the images
+dir_path = "/Users/lawrencejesudasan/Downloads/Watches_Images_Processed"
 
-# Create the grayscale directory if it does not exist
-if not os.path.exists("grayscale"):
-    os.makedirs("grayscale")
+# Create an empty DataFrame with columns for the image filename and the color histogram features
+df_new = pd.DataFrame(columns=["filename", "features"])
 
-# Loop through each subdirectory in the directory path
-for subdir in os.listdir(dir_path):
-    # Create the grayscale subdirectory if it does not exist
-    if not os.path.exists(os.path.join("grayscale", subdir)):
-        os.makedirs(os.path.join("grayscale", subdir))
-    # Loop through each image in the subdirectory
-    for filename in os.listdir(os.path.join(dir_path, subdir)):
+# Loop through each subfolder in the directory
+for subfolder in os.listdir(dir_path):
+    subfolder_path = os.path.join(dir_path, subfolder)
+    # Check if the subfolder is actually a directory
+    if not os.path.isdir(subfolder_path):
+        continue
+    # Loop through each image in the subfolder
+    for filename in os.listdir(subfolder_path):
+        # Ignore non-image files and the .DS_Store file
+        if not filename.endswith(('.jpg', '.jpeg', '.png')) or filename == '.DS_Store':
+            continue
+        # Ignore files that don't start with "processed_"
+        if not filename.startswith('processed_'):
+            continue
+        # Extract the reference name by removing the "processed_" prefix and file extension
+        reference = os.path.splitext(filename[len('processed_'):])[0]
         # Load the image
-        img = cv2.imread(os.path.join(dir_path, subdir, filename))
+        img = Image.open(os.path.join(subfolder_path, filename))
         # Convert the image to grayscale
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        gray = img.convert('L')
         # Compute the color histogram features
-        hist = cv2.calcHist([gray], [0], None, [64], [0, 64])
-        hist = cv2.normalize(hist, hist)
+        hist = np.array(gray.histogram())
+        hist = hist / np.sum(hist)  # Normalize the histogram so that the values sum to 1
         # Reshape the feature vector to a 1D array
-        features = np.reshape(hist, (-1,))
-        # Save the features in the grayscale directory with the same filename
-        np.save(os.path.join("grayscale", filename[:-4] + ".npy"), features)
+        features = hist.reshape(-1)
+        # Add the new row to the DataFrame of new data
+        df_new = pd.concat([df_new, pd.DataFrame({"filename": [reference], "features": [features]})], ignore_index=True)
+
+# Load your existing DataFrame from the "data_with_images.csv" file
+df_existing = pd.read_csv("5_data_with_images.csv")
+
+# Merge the new DataFrame with the existing DataFrame using the reference name column as the link
+df_merged = pd.merge(df_existing, df_new, left_on="Reference", right_on="filename")
+
+# Drop the redundant "filename" column
+df_merged.drop("filename", axis=1, inplace=True)
+
+# Save the merged DataFrame to the existing file
+df_merged.to_csv("5_data_with_images.csv", index=False)
