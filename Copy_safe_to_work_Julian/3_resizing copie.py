ecf497b99c586da2aacbfe1b85cb5e14a2a41501
Jmat0 <104056051+Jmat0@users.noreply.github.com>
2023-05-24 11:22:48 +0200

    yo

---
+++ Copy_safe_to_work_Julian/3_resizing copie.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import os
+
+# Define the path to your images directory
+images_dir = '/Users/lawrencejesudasan/Downloads/Watches_Images_Processed'
+
+# Define the desired image size for preprocessing
+image_size = (350, 350)
+
+# Loop through all the subdirectories in the directory
+for root, dirs, files in os.walk(images_dir):
+    # Loop through all the image files in the subdirectory
+    for filename in files:
+        if filename.endswith('.jpg') or filename.endswith('.png'): # check if the file is an image file
+            # Load the image
+            img = Image.open(os.path.join(root, filename))
+            # Resize the image while maintaining aspect ratio
+            img.thumbnail(image_size, Image.ANTIALIAS)
+            # Convert the image to RGB mode
+            rgb_img = img.convert('RGB')
+            # Save the resized image with a new filename
+            new_filename = 'processed_' + filename
+            rgb_img.save(os.path.join(root, new_filename), 'JPEG')
+            # Delete the original image
+            os.remove(os.path.join(root, filename))
