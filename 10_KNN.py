99e7b0dc30c97612693528ccb6cdb1d4fb772db4
Jmat0 <104056051+Jmat0@users.noreply.github.com>
2023-06-02 11:59:50 +0200

    w

--- 10_KNN.py
+++ 10_KNN.py
@@ -1,19 +1,49 @@
+import pandas as pd
+import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
+# Read the CSV file into a pandas DataFrame
+data = pd.read_csv('8a_df_gray.csv')
 
-knn = KNeighborsClassifier(n_neighbors=3)  # Specify the number of neighbors (K)
-knn.fit(X_train, y_train)  # X_train represents the feature matrix, y_train represents the target vector
+# Extract the gray features and reference names into separate arrays
+gray_features = data['gray_features'].apply(lambda x: np.fromstring(x[1:-1], sep=' ')).values
+reference_names = data['filename'].values
 
-# Split the dataset into training and testing subsets
-X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+# Reshape the gray features to a 2D array
+X = np.vstack(gray_features)
 
-# Create a KNN classifier
-knn = KNeighborsClassifier(n_neighbors=3)
+# Create the target vector y with the reference names
+y = reference_names
 
-# Train the KNN model
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize the KNN classifier
+knn = KNeighborsClassifier(n_neighbors=2)
+
+# Train the KNN model using only the gray features
 knn.fit(X_train, y_train)
 
-# Evaluate the model
-accuracy = knn.score(X_test, y_test)
-print("Accuracy:", accuracy)
+# Predict the closest neighbors for a specific watch (example)
+watch_features = gray_features[0]  # Replace with the features of the watch you want to find neighbors for
+
+# Transform the watch_features to a 2D array
+#watch_features = np.reshape(watch_features, (1, 0))
+
+closest_neighbors = knn.kneighbors([watch_features], n_neighbors=3)
+
+# Predict the labels for the training data
+y_train_pred = knn.predict(X_train)
+
+
+# Retrieve the indices of the closest neighbors
+neighbor_indices = closest_neighbors[1][0]
+
+# Retrieve the unique identifiers of the closest neighbors
+closest_watch_ids = [reference_names[idx] for idx in neighbor_indices]
+
+print("Closest neighbors:", closest_watch_ids)
+
+# Print the predicted labels
+#print(y_train_pred)
